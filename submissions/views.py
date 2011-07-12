10e0f816cbc18cc0ee853dc2305daf1226ad1d07
Dev Server <dev@dev.com>
2011-07-12 20:31:48 +0000

    simplified http rendering

--- submissions/views.py
+++ submissions/views.py
@@ -1,11 +1,6 @@
-from django.template import Context, loader
+from django.shortcuts import render_to_response
 from submissions.models import Submission
-from django.http import HttpResponse
 
 def index(request):
 	subs = Submission.objects.all()
-	t = loader.get_template('submissions/index.html')
-	c = Context({
-		'list': subs,
-	})
-	return HttpResponse(t.render(c))
+	return render_to_response('submissions/index.html', {'list': subs})
