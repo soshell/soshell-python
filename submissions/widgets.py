ca99f27703eeb40f7281caa2753ab9827df23b0f
adamjon858 <adamjbratt@gmail.com>
2011-07-19 14:02:44 -0400

    hopefully fixed widget error

--- submissions/widgets.py
+++ submissions/widgets.py
@@ -3,21 +3,19 @@ from django.utils.encoding import smart_str
 from django.utils.safestring import mark_safe
 from django.forms.util import flatatt
 
-class CommaTags(Widget):
+class CommaTags(forms.Widget):
     def render(self, name, value, attrs=None):
         final_attrs = self.build_attrs(attrs, type='text', name=name)
-        objects = []
-        for each in value:
-            try:
-                object = Tag.objects.get(pk=each)
-            except:
-                continue
-            objects.append(object)
-
-        values = []
-        for each in objects:
-            values.append(str(each))
-        value = ', '.join(values)
-        if value: # only add 'value' if it's nonempty
-            final_attrs['value'] = force_unicode(value)
+        if not type(value) == unicode:
+            values = []
+            for each in value:
+                try:
+                    values.append(str(Tag.objects.get(pk=each).name))
+                except:
+                    continue
+            value = ', '.join(values)
+            if value: # only add 'value' if it's nonempty
+                final_attrs['value'] = smart_str(value)
+        else:
+            final_attrs['value'] = smart_str(value)
         return mark_safe(u'<input%s />' % flatatt(final_attrs))
